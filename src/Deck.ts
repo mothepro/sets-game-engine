25dd319f5fa60345acb4fff4637957c2da894e7b
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2018-04-12 01:18:36 -0700

    Deck's market should be immutable.

--- src/Deck.ts
+++ src/Deck.ts
@@ -6,13 +6,17 @@ export default class Deck {
     protected cards: Card[] = []
 
     /** The cards shown the players. */
-    public market: Card[] = []
+    protected market_: Card[] = []
 
     /** The number of cards that are shown in the market. */
     protected static readonly MARKET_SIZE = 9
 
     /** The number of cards that the market should increment by each time. */
     protected static readonly MARKET_INC = 3
+
+    get market(): ReadonlyArray<Card> {
+        return this.market_
+    }
 
     constructor(shoe = 1) {
         for(let i = 0; i < Card.COMBINATIONS * shoe; i++)
@@ -26,30 +30,30 @@ export default class Deck {
         while(
             this.cards.length &&
             (
-                this.market.length < Deck.MARKET_SIZE ||
-                !Card.hasSet(this.market)
+                this.market_.length < Deck.MARKET_SIZE ||
+                !Card.hasSet(this.market_)
             )
         ) {
             for(let i = 0; i < Math.min(Deck.MARKET_INC, this.cards.length); i++)
-                this.market.push(this.cards.shift()!)
+                this.market_.push(this.cards.shift()!)
         }
     }
 
     /** Whether any more sets can be made. */
     public isDone(): boolean {
-        return this.cards.length === 0 && !Card.hasSet(this.market)
+        return this.cards.length === 0 && !Card.hasSet(this.market_)
     }
 
     /** Pops a Set from the Market. */
     public removeSet(indexs: SetIndexs): Set {
         const ret = []
         for(const index of indexs) {
-            ret.push(this.market[index])
-            delete this.market[index]
+            ret.push(this.market_[index])
+            delete this.market_[index]
         }
 
         // remove non cards
-        this.market = this.market.filter(card => card instanceof Card)
+        this.market_ = this.market_.filter(card => card instanceof Card)
 
         return ret as Set
     }
