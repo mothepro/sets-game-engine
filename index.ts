bcf64c37b3a003a76dec5962b98e7d20280f79e4
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2019-12-09 11:56:24 -0800

    Export the game as the default

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 export { default as Details } from './src/Details.js'
 export { default as Card, CardSet } from './src/Card.js'
 export { default as Player } from './src/Player.js'
-export { default as Game } from './src/Game.js'
+export { default } from './src/Game.js'
