44298e8256221829b8438e4b4698c92123125cca
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2019-12-24 14:01:53 -0600

    Fixed Details not having a default export

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
-export { default as Details } from './src/Details.js'
 export { default as Card, CardSet } from './src/Card.js'
 export { default as Player } from './src/Player.js'
 export { default } from './src/Game.js'
+// One liner doesn't work with TS 3.7.3
+import * as Details from './src/Details.js'
+export { Details }
