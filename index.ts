c2371172a8980d446fcdcda92261aeae3beecfce
Maurice Prosper <mothepro@mothepro-macbookpro3.roam.corp.google.com>
2020-04-08 03:23:31 -0500

    Update deps

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 export { default as Card, CardSet } from './src/Card.js'
 export { default as Player } from './src/Player.js'
 export { default } from './src/Game.js'
-// One liner doesn't work with TS 3.7.3
-import * as Details from './src/Details.js'
-export { Details }
+export * as Details from './src/Details.js'
